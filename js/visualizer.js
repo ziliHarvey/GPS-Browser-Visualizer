8ce96c2b11bf96aeefae08f14c4d01bf4cc910e2
ziliHarvey <zili@andrew.cmu.edu>
2020-01-28 21:26:55 -0500

    switching between satellite and map functionality finished

--- js/visualizer.js
+++ js/visualizer.js
@@ -23,11 +23,17 @@ ros.on('close', function() {
 });
 
 // map
+var layerStyle = "satellite-v8";
+
 L.mapbox.accessToken = 'pk.eyJ1IjoiemlsaWhhcnZleSIsImEiOiJjazVuYnFpbDYxOHRhM2tvMXFyMDh6eGJyIn0.ABWx_KoY0Zrc1aIsSU0XNQ';
-var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([0, 0], 3)
-    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/satellite-v8'));;
+// var map = L.mapbox.map('map', 'mapbox.streets', {fadeAnimation: false, minZoom:3})
+//     .setView([0, 0], 3)
+//     .addLayer(L.mapbox.styleLayer("mapbox://styles/mapbox/" + layerStyle));;
+var map = L.mapbox.map('map', null, {fadeAnimation: false, minZoom:3})
+    .setView([0, 0], 3);
 var polyline = L.polyline([]).addTo(map);
+var layerGroup = L.layerGroup().addTo(map);
+layerGroup.addLayer(L.mapbox.styleLayer("mapbox://styles/mapbox/" + layerStyle));
 
 function draw() {
     polyline.addLatLng(center);
@@ -55,3 +61,12 @@ listener.subscribe(function(message) {
     if (center)
         draw();
 });
+
+function switchLayer(layerId) {
+    if (layerId == 1) {
+        layerStyle = "satellite-v8";
+    } else {
+        layerStyle = "streets-v11";
+    }
+    layerGroup.clearLayers().addLayer(L.mapbox.styleLayer("mapbox://styles/mapbox/" + layerStyle));
+}
